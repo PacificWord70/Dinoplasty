69ee378fd4a4497bdab5733a74d7367af722d00b
Heidi Anderson <heidianderson@heidis-mbp.coldspring.marian.edu>
2017-06-07 16:27:02 -0400

    added dinos to unordered list

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const app = {
-    init(formSelector){
+    init(selectors){
         this.max=0
+        this.list = document.querySelector(selectors.listSelector)
         document
-            .querySelector(formSelector)
+            .querySelector(selectors.formSelector)
             .addEventListener('submit', this.addDino.bind(this))
     },
 
@@ -14,9 +15,19 @@ const app = {
             id: ++this.max
         }
 
-        console.log(dino.name+":"+dino.id)
+        this.list.appendChild(this.renderListItem(dino))
+
         ev.target.dinoName.value = ''
     },
+
+    renderListItem(dino){
+        const listItem = document.createElement('li')
+        listItem.textContent = dino.name
+        return listItem
+    }
 }
 
-app.init('#dino-form')
+app.init({
+    formSelector: '#dino-form',
+    listSelector: '#dino-list',
+})
